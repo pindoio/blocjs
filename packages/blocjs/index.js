d99cf293784b830aabf68ca271f49e942ac73133
Eugene Rwagasore <rwagasore@gmail.com>
2019-11-22 20:11:41 +0100

    Fix: Corrected the import of component to have Hide available

--- packages/blocjs/index.js
+++
@@ -1,6 +0,0 @@
-import Box from "./src/box";
-import Hide from "./src/hide";
-import Flex from "./src/flex";
-import Bloc from "./src/bloc";
-
-export { Box, Hide, Flex, Bloc };
--- packages/blocjs/src/index.js
+++ packages/blocjs/src/index.js
@@ -1,6 +1,7 @@
 import Box from "./box";
+import Hide from "./hide";
 import Flex from "./flex";
 import Bloc from "./bloc";
 import Text from "./text";
 
-export { Box, Flex, Bloc, Text };
+export { Box, Hide, Flex, Bloc, Text };
