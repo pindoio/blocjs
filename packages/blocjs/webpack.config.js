cf0752355e6d0f39031f3078413939c86ebbbd72
Eugene Rwagasore <1290048+erwagasore@users.noreply.github.com>
2020-07-10 21:56:00 +0200

    fix: Forced the bundle to use commonjs2 (#46)
    * Forced the bundle to use commonjs2
    
    * Reverted back
    
    * fix: bundled as commonjs package

--- packages/blocjs/webpack.config.js
+++ packages/blocjs/webpack.config.js
@@ -1,20 +1,21 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
   module: {
     rules: [
       {
-	test: /\.js$/,
-	exclude: /(node_modules)/,
+        test: /\.js$/,
+        exclude: /(node_modules)/,
         use: {
-	  loader: 'babel-loader',
-	},
+          loader: "babel-loader"
+        }
       }
     ]
   },
   entry: "./src/index.js",
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, './dist')
+    libraryTarget: "commonjs2",
+    path: path.resolve(__dirname, "./dist")
   }
 };
